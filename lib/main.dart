383a016e165ce88263031fd44537398691f24904
Ash Davies <ashdavies@users.noreply.github.com>
2018-08-27 22:18:30 +0200

    Implement material design backdrop

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,13 @@ void main() => runApp(new Investors());
 
 class Investors extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Startup Name Generator',
-      theme: new ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: new RandomWords(),
-    );
-  }
+  Widget build(BuildContext context) => new MaterialApp(
+        title: 'Private Investors Portal',
+        theme: new ThemeData(
+          primaryColor: Colors.blue,
+        ),
+        home: new RandomWords(),
+      );
 }
 
 class RandomWords extends StatefulWidget {
@@ -21,15 +19,39 @@ class RandomWords extends StatefulWidget {
   State<StatefulWidget> createState() => new RandomWordsState();
 }
 
-class RandomWordsState extends State<RandomWords> {
+class RandomWordsState extends State<RandomWords>
+    with SingleTickerProviderStateMixin {
+  static const _PANEL_HEADER_HEIGHT = 32.0;
+
   final List<WordPair> _suggestions = <WordPair>[];
   final Set<WordPair> _saved = new Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new AnimationController(
+      duration: const Duration(milliseconds: 100),
+      value: 1.0,
+      vsync: this,
+    );
+  }
+
   @override
   Widget build(BuildContext context) => new Scaffold(
         appBar: new AppBar(
-          title: new Text("Startup Name Generator"),
+          elevation: 0.0,
+          title: new Text("Private Investors Portal"),
+          leading: new IconButton(
+              onPressed: () {
+                _controller.fling(velocity: _isPanelVisible ? -1.0 : 1.0);
+              },
+              icon: new AnimatedIcon(
+                icon: AnimatedIcons.close_menu,
+                progress: _controller.view,
+              )),
           actions: <Widget>[
             new IconButton(
               icon: const Icon(Icons.list),
@@ -37,37 +59,107 @@ class RandomWordsState extends State<RandomWords> {
             )
           ],
         ),
-        body: _buildSuggestions(),
+        body: new SafeArea(
+          child: new LayoutBuilder(
+            builder: _buildStack,
+          ),
+        ),
       );
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-            (WordPair pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile
-              .divideTiles(
-                context: context,
-                tiles: tiles,
-              )
-              .toList();
+  bool get _isPanelVisible {
+    final AnimationStatus status = _controller.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
 
-          return new Scaffold(
-            appBar: new AppBar(
-              title: const Text('Saved Suggestions'),
+  void _pushSaved() => Navigator.of(context).push(
+        new MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            final Iterable<ListTile> tiles = _saved.map(
+              (WordPair pair) {
+                return new ListTile(
+                  title: new Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+            );
+            final List<Widget> divided = ListTile
+                .divideTiles(
+                  context: context,
+                  tiles: tiles,
+                )
+                .toList();
+
+            return new Scaffold(
+              appBar: new AppBar(
+                title: const Text('Saved Suggestions'),
+              ),
+              body: new ListView(children: divided),
+            );
+          },
+        ),
+      );
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+    final Animation<RelativeRect> animation = _getPanelAnimation(constraints);
+    final ThemeData theme = Theme.of(context);
+    return new Container(
+        color: theme.primaryColor,
+        child: new Stack(
+          children: <Widget>[
+            new Center(
+              child: new Text("base"),
             ),
-            body: new ListView(children: divided),
-          );
-        },
+            new PositionedTransition(
+                rect: animation,
+                child: new Material(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: const Radius.circular(16.0),
+                      topRight: const Radius.circular(16.0),
+                    ),
+                    elevation: 12.0,
+                    child: new Column(
+                      children: <Widget>[
+                        new Container(
+                          height: _PANEL_HEADER_HEIGHT,
+                          child: new Center(
+                            child: new Text("panel"),
+                          ),
+                        ),
+                        new Expanded(
+                          child: new Center(
+                            child: _buildSuggestions(),
+                          ),
+                        ),
+                      ],
+                    )
+                )
+            ),
+          ],
+        ));
+  }
+
+  Animation<RelativeRect> _getPanelAnimation(BoxConstraints constraints) {
+    final double height = constraints.biggest.height;
+    final double top = height - _PANEL_HEADER_HEIGHT;
+    final double bottom = -_PANEL_HEADER_HEIGHT;
+
+    return new RelativeRectTween(
+      begin: new RelativeRect.fromLTRB(0.0, top, 0.0, bottom),
+      end: new RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+    )
+        .animate(
+      new CurvedAnimation(
+        parent: _controller,
+        curve: Curves.linear,
       ),
     );
   }
