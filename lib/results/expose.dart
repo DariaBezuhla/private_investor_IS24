711e3604d1540803acbbc1ac74502f525cc25415
jakepanter <jakob@panten-sh.de>
2020-04-28 09:56:07 +0200

    setup fresh blank project to start from scratch

--- lib/results/expose.dart
+++
@@ -1,9 +0,0 @@
-class Expose {
-  final String picture;
-
-  Expose({this.picture});
-
-  factory Expose.fromJson(Map<String, dynamic> json) => Expose(
-        picture: json['sections'][0]['media'][0]['fullImageUrl'],
-      );
-}
